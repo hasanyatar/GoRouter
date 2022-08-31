62fc9fbdfc9ac4800b0116e6e546b7eacd8f830f
hsnytr99 <hsnytr99@gmail.com>
2022-08-31 17:56:25 +0300

    Initialize GoRouter

---
+++ app/lib/views/page_3.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 3'),
+      ),
+      body: const Center(
+        child: Text('Page 3'),
+      ),
+    );
+  }
+}
---
+++ app/lib/views/page_4.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Page4 extends StatelessWidget {
+  const Page4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 4'),
+      ),
+      body: const Center(
+        child: Text('Page 4'),
+      ),
+    );
+  }
+}
